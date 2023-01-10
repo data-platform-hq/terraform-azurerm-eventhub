de555591a1147a1d136c8e98276850d585e124d3
Oleh Mykolaishyn <owlleg6@gmail.com>
2023-01-10 22:22:51 +0200

    fix: added topics output

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,13 @@ output "namespace_endpoint" {
   value       = "sb://${azurerm_eventhub_namespace.this.name}.servicebus.windows.net/"
   description = "Event Hub namespace endpoint"
 }
+
+output "topic" {
+  value = length(var.eventhub_topic) == 0 ? {} : {
+    for topic, params in var.eventhub_topic : azurerm_eventhub.this[topic].name => {
+      topic_id                  = azurerm_eventhub.this[topic].id,
+      primary_key               = azurerm_eventhub_authorization_rule.this[topic].primary_key
+      primary_connection_string = azurerm_eventhub_authorization_rule.this[topic].primary_connection_string
+    }
+  }
+}
